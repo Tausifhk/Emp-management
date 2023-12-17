e6087dcfe3bd2e4202997a13dde330eaa5a03575
Tausifhk <132562821+Tausifhk@users.noreply.github.com>
2023-12-17 09:52:58 +0530

    Update script.js

--- EMsystem/src/main/webapp/script.js
+++ EMsystem/src/main/webapp/script.js
@@ -7,7 +7,7 @@
         if (root.classList.contains("dark-mode")) {
             root.style.setProperty('--secondary-color', '#1c1c1c');
             darkModeButton.value = "Light Mode";
-            localStorage.setItem('true', isDarkMode);
+            localStorage.setItem('darkMode', 'true');
         } else {
             root.style.setProperty('--secondary-color', '#ffffff');
             darkModeButton.value = "Dark Mode";
